c1e4f44249aabace7a56937dd793d809db15a539
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-03-06 20:46:13 +0900

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const importBtn = document.getElementById("btn-import");
+  const viewBtn = document.getElementById("btn-view");
 
   if (importBtn) {
     importBtn.addEventListener("click", () => {
 
       window.location.href = "data_source.html";
+
+    });
+  }
+
+  if (viewBtn) {
+    viewBtn.addEventListener("click", () => {
+
+      window.location.href = "data_view.html";
 
     });
   }
